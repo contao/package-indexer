243fc0e97ddceca38a59e7c23955122e01b7e9ba
Yanick Witschi <yanick.witschi@terminal42.ch>
2018-08-21 17:31:16 +0200

    Updated deps

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Algolia\SearchBundle\AlgoliaSearchBundle::class => ['all' => true],
     EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
